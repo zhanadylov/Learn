1818c01862ab364a3a679aafa2bc57faa65af4d7
Marsel <zhanadylov.kg@gmail.com>
2020-11-02 21:16:12 +0600

    second

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
 "use strict";
 
-let incr = 10,
-    decr = 10;
-    console.log(++incr);
+const numberOfFilms = +prompt('How many films did you watch yet?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
+
+const a = prompt('One of the last watched films?',''),
+      b = prompt('Your vote?',''),
+      c = prompt('One of the watched films?',''),
+      d = prompt('Your vote?','');
+
+      personalMovieDB.movies[a] = b;
+      personalMovieDB.movies[c] = d;
+
+      console.log(personalMovieDB);
