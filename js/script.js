816beae4d6b2ace675af50bc89d0d4673db5c10e
Marsel <zhanadylov.kg@gmail.com>
2021-01-18 23:07:46 +0600

    fsdf

--- js/script.js
+++ js/script.js
@@ -1,27 +1,11 @@
 "use strict";
+let NumberOfFilms;
 
-const numberOfFilms = +prompt('How many films did you watch yet?', '');
+function start(){
+    NumberOfFilms = +prompt('how many films did you watched?', '');
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    private: false
-};
-
-for(let i = 0; i < 2; i++){
-    const a = prompt('One of the last watched films?',''),
-      b = prompt('Your vote?','');
-
-      if(a != null && b != null && a != '' && b != '' && a.length < 50){
-        personalMovieDB.movies[a] = b;
-        console.log.log("done");
-      }else{
-          console.log("error!");
-          i--;
-      }
-      
+    while (NumberOfFilms == '' || NumberOfFilms == null || isNaN(NumberOfFilms)){
+        NumberOfFilms = +prompt('how many films did you watched?', '');
+    }
 }
-      console.log(personalMovieDB);
-    
+start();
