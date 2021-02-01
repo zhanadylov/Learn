5fbe2098e39d533ee3b6fa3615d1dfe79e58d546
Marsel <zhanadylov.kg@gmail.com>
2021-02-01 14:36:36 +0600

    last changes

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ function start(){
 }
 start();
 
-const personalMovieDb = {
+const personalMovieDB = {
     count: NumberOfFilms,
     movies: {},
     actors: {},
@@ -18,16 +18,44 @@ const personalMovieDb = {
     private: false
 };
 
-for (let i = 0; i < 2; i++){
-    const a = prompt('Last watched film?',''),
-    b = prompt('your vote?','');
-
-    if(a != null && b != null && a != '' && b != '' && a.length < 50){
-        personalMovieDb.movies[a] = b;
-        console.log('done');
+ function rememberMyFilms(){
+    for (let i = 0; i < 2; i++){
+        const a = prompt('Last watched film?',''),
+        b = prompt('your vote?','');
+    
+        if(a != null && b != null && a != '' && b != '' && a.length < 50){
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        }
+        else {
+            console.log('error');
+            i--;
+        }
     }
-    else {
-        console.log('error');
-        i--;
+ }
+
+ function detectPersonalLevel(){
+     if(personalMovieDB.count < 10){
+         console.log("Less movie watched");
+     }else if (personalMovieDB.count >=10 && personalMovieDB.count < 30){
+         console.log("you are the movieman!");
+    }else {
+        console.log("Error");
+    }
+ }
+
+ function showMyDB (hidden){
+     if (!hidden){
+         console.log(personalMovieDB);
+     }    
+}
+
+showMyDB(personalMovieDB.private);
+
+function writeYourGenres(){
+    for(let i = 1; i <= 3; i++){
+        personalMovieDB.genres[i -1] = prompt('Your likly genre ${i}');
     }
 }
+
+writeYourGenres();
